ac8eecda9c2c9d529ab6284dd4af6ecf0ba40b2b
Maximal <maxim@infofree.ru>
2018-03-09 17:24:43 +0500

    add express server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello world!')
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server is up on port ${PORT}`)
+})
