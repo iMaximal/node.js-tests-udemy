3cda6b1a65eaafa940d1e785bfb3181f141bee36
Maximal <maxim@infofree.ru>
2018-03-09 18:05:49 +0500

    add simple express tests

--- server/server.js
+++ server/server.js
@@ -5,10 +5,31 @@ const PORT = process.env.PORT || 3000
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello world!')
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'Todo App v1.0',
+  })
+})
+
+
+// GET /users
+// Give users a name prop and age prop
+app.get('/users', (req, res) => {
+  res.send([{
+    name: 'Max',
+    age: 30,
+  }, {
+    name: 'Andrew',
+    age: 25,
+  }, {
+    name: 'Jen',
+    age: 26,
+  }])
 })
 
 
 app.listen(PORT, () => {
   console.log(`Server is up on port ${PORT}`)
 })
+
+module.exports.app = app
---
+++ server/server.test.js
@@ -0,0 +1,32 @@
+const request = require('supertest')
+const expect = require('expect')
+
+const app = require('./server').app
+
+it('should return 404 response', (done) => {
+  request(app)
+    .get('/')
+    .expect(404)
+    .expect((res) => {
+      expect(res.body).toInclude({
+        error: 'Page not found.',
+      })
+    })
+    .end(done)
+})
+
+// Make a new test
+// assert 200
+// Assert that you exist in users array
+it('should return my user object', (done) => {
+  request(app)
+    .get('/users')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body).toInclude({
+        name: 'Max',
+        age: 30,
+      })
+    })
+    .end(done)
+})
