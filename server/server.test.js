8fa9df6fea5949ed77af47d596046717ffe189e6
Maximal <maxim@infofree.ru>
2018-03-09 18:34:04 +0500

    add Descriptions for tests

--- server/server.test.js
+++ server/server.test.js
@@ -3,30 +3,36 @@ const expect = require('expect')
 
 const app = require('./server').app
 
-it('should return 404 response', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        error: 'Page not found.',
-      })
+describe('Server', () => {
+  describe('GET /', () => {
+    it('should return 404 response', (done) => {
+      request(app)
+        .get('/')
+        .expect(404)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            error: 'Page not found.',
+          })
+        })
+        .end(done)
     })
-    .end(done)
-})
+  })
 
-// Make a new test
-// assert 200
-// Assert that you exist in users array
-it('should return my user object', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        name: 'Max',
-        age: 30,
-      })
+  // Make a new test
+  // assert 200
+  // Assert that you exist in users array
+  describe('GET /users', () => {
+    it('should return my user object', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            name: 'Max',
+            age: 30,
+          })
+        })
+        .end(done)
     })
-    .end(done)
+  })
 })
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,48 +1,53 @@
 const expect = require('expect')
 const utils = require('./utils')
 
-it('it should add two numbers', () => {
-  const res = utils.add(33, 11)
+describe('Utils', () => {
+  describe('#add', () => {
+    it('it should add two numbers', () => {
+      const res = utils.add(33, 11)
 
-  expect(res).toBe(44).toBeA('number')
-})
+      expect(res).toBe(44).toBeA('number')
+    })
 
 
-it('should async add two numbers', (done) => {
-  utils.asyncAdd(4, 3, (sum) => {
-    expect(sum).toBe(7).toBeA('number')
-    done()
+    it('should async add two numbers', (done) => {
+      utils.asyncAdd(4, 3, (sum) => {
+        expect(sum).toBe(7).toBeA('number')
+        done()
+      })
+    })
   })
-})
 
 
-it('should square a number', () => {
-  const res = utils.square(3)
+  describe('#square', () => {
+    it('should square a number', () => {
+      const res = utils.square(3)
 
-  expect(res).toBe(9).toBeA('number')
-})
+      expect(res).toBe(9).toBeA('number')
+    })
 
 
-it('should async square a number', (done) => {
-  utils.asyncSquare(5, (res) => {
-    expect(res).toBe(25).toBeA('number')
-    done()
+    it('should async square a number', (done) => {
+      utils.asyncSquare(5, (res) => {
+        expect(res).toBe(25).toBeA('number')
+        done()
+      })
+    })
   })
-})
 
+  // should verify first and last names are set
+  // assert it includes firstName and lastName with proper values
+  it('should set firstName and lastName', () => {
+    const user = {
+      location: 'Yekaterinburg',
+      age: 30,
+    }
+    const res = utils.setName(user, 'Maksim Golitsyn')
 
-// should verify first and last names are set
-// assert it includes firstName and lastName with proper values
-it('should set firstName and lastName', () => {
-  const user = {
-    location: 'Yekaterinburg',
-    age: 30,
-  }
-  const res = utils.setName(user, 'Maksim Golitsyn')
-
-  expect(res).toInclude({
-    firstName: 'Maksim',
-    lastName: 'Golitsyn',
+    expect(res).toInclude({
+      firstName: 'Maksim',
+      lastName: 'Golitsyn',
+    })
   })
 })
 
